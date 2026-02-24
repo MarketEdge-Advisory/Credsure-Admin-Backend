c5bf1b6077253d9ab9dea63a0f98f227a3f24949
Timilehin Abodunrin <joeltimmy7@gmail.com>
2026-02-24 22:22:23 +0100

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,16 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://credsure-landingpage.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
